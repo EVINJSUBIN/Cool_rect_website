9346f4f28bf767f87126544aad30a8fec0044bde
EVINJSUBIN <youridertech@gmail.com>
2024-08-15 12:50:00 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   build/index.html
    	modified:   package.json
    	modified:   vite.config.js
    
    gt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Cool_rect_website', // Replace with your repository name
   plugins: [react()],
-})
+});
